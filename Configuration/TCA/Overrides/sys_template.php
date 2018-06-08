b830276abdb0fb758d8c4c742d355ac42f17f459
Ioulia Kondratovitch <ik@plan2.net>
2018-06-08 17:29:53 +0200

    [TASK] move addStaticFile to  ext.tables.php (best practice)

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,5 @@
+<?php
+
+defined('TYPO3_MODE') or die('Access denied.');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('news_workflow', 'Configuration/TypoScript', 'News Workflow');
--- ext_tables.php
+++ ext_tables.php
@@ -6,6 +6,3 @@ defined('TYPO3_MODE') or die('Access denied.');
     'WorkflowController::copyNews',
     'Plan2net\\NewsWorkflow\\Controller\\WorkflowController->renderAjax'
 );
-
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'News Workflow');
